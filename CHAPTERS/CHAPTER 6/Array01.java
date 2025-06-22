918dcecc34427b6e12455724eee9e011402f2dc4
Republic Oba <republicoba1@gmail.com>
2025-06-22 15:05:39 +0100

    new works

---
+++ CHAPTERS/CHAPTER 6/Array01.java
@@ -0,0 +1,13 @@
+public class Array01{
+	public static void main(String[] args){
+		int[] r = {0, 0, 0, 0, 0, 0, 0, 0, 0, 9};
+		for(int i = 0; i < r.length; i++){
+			System.out.println(r[i]);
+		}
+
+		int[] g = new int[6];
+		for(int i = 0; i < g.length; i++){
+			System.out.println(-6);
+		}
+	}
+}
